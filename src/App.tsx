0f9de419ec411da36775ad9055f5a53880f44a80
Jomar <jomar.magno026@gmail.com>
2021-11-08 22:19:28 +0800

    Setup Material-UI library

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
 function App() {
-  return <div>Hello from Bookings app</div>;
+  return (
+    <div>
+      <Button variant='contained'>Hello World</Button>
+    </div>
+  );
 }
 
 export default App;
